661e22109ad315a9d815eba7b454abb2f5f94cbf
Thiago Rodrigues <thiagorcefetmg@gmail.com>
2020-12-03 21:36:40 -0300

    add processor1

--- processor1.v
+++ processor1.v
@@ -11,7 +11,6 @@ module processor1(processor_index, clock, reset, start, listen, p1, op, block, t
     
     wire hit, wb_m1, wb_m2;
     wire [1:0] state_m1, state_m2, processor_m1, processor_m2;
-    wire [2:0] bus;
     wire [7:0] data_m2;
 
     reg w, wb, active_m1, active_m2, cache_hit;
@@ -61,6 +60,7 @@ module processor1(processor_index, clock, reset, start, listen, p1, op, block, t
                                 //read hit
                                 if (tag_p1[block] == tag_in && state_p1[block] != 2'b00) begin
                                     cpu_action = 3'b001;
+                                    w = 1'b1;
                                 end
                                 //read miss
                                 else begin
@@ -91,7 +91,6 @@ module processor1(processor_index, clock, reset, start, listen, p1, op, block, t
                     end
                     step = 2'b01;
                 end
-
                 2'b01: begin
                     //mudança de estado do processador que gera a ação
                     if(processor_index == processor_m1) begin
@@ -100,7 +99,7 @@ module processor1(processor_index, clock, reset, start, listen, p1, op, block, t
                         state_p1[block] = state_m1;
                         tag_p1[block] = (cpu_action == 3'b010 && bus_in[10] == 1'b1) ? tag_in : tag_p1[block];
                         data_p1[block] = (cpu_action == 3'b011 || cpu_action == 3'b100) ? wr_data : data_p1[block];
-                        data_p1[block] = (cpu_action == 3'b010 && bus_in[10] == 1'b1) ? data_m2 : data_p1[block];
+                        data_p1[block] = (cpu_action == 3'b010 && bus_in[10] == 1'b1 && w != 1'b1) ? data_m2 : data_p1[block];
                         bus_out = {wb, w, state_p1[block], data_p1[block]};
                     end
                     //mudança de estado dos processadores que escutam o barramento
@@ -112,12 +111,9 @@ module processor1(processor_index, clock, reset, start, listen, p1, op, block, t
                         bus_out = {wb, hit, state_p1[block], data_p1[block]};
                     end
                 end
-                2'b10: begin
-                    step = 2'b0;
-                end
             endcase
         end
     end
     state_machine1 sm1(clock, active_m1, cpu_action, state_p1[block], p1, wb_m1, state_m1, bus_m1, processor_m1);
-    state_machine2 sm2(clock, active_m2, cache_hit, state_p1[block], p1, bus_m1_in, data_p1[block], wb_m2, abort_mem_accs, hit, state_m2, processor_m2, data_m2);
+    state_machine2 sm2(reset, active_m2, cache_hit, state_p1[block], p1, bus_m1_in, data_p1[block], wb_m2, abort_mem_accs, hit, state_m2, processor_m2, data_m2);
 endmodule
